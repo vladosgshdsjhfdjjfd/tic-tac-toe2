9c7dfa3d5de75b4bbcb7c6a14c376afd50fe3c56
vladosgshdsjhfdjjfd <Vlad5humilin@yandex.ru>
2023-09-26 13:03:16 +0300

    my commit

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double R1, R2, R3;
+	cout << "Enter R1 : ";
+	cin >> R1;
+	cout << "Enter R2 : ";
+	cin >> R2; 
+	cout << "Enter R3 : ";
+	cin >> R3;
+	double R0 = 1 / R1 + 1 / R2 + 1 / R3;
+	cout << "R0 = " << R0 << "\n";
+	return 0;
+}
